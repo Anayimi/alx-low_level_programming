ccaf4715ce6ab3e816d8f3acf5f110998abe4e29
Anayimi <ogievaanayimi@gmail.com>
2022-09-14 01:17:43 -0700

    Checks for alphabetic character

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an integer
+* @n: The integer to be computed
+* Return: int or int negative.
+*/
+int _abs(int)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
