6976006b8f95f0e3153048a5402f3f9333762439
Anayimi <ogievaanayimi@gmail.com>
2022-09-14 10:04:19 -0700

    Prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,43 +13,21 @@ void times_table(void)
 	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (c = 1; c <= 9; c++)
-		{
-			d = (r * c);
-			if ((d / 10) > 0)
-			{
-				_putchar((d / 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-			_putchar((d % 10) + '0');
-			if (c < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
-}
 
-		for (word = 1; word <= 9; word++)
+		for (c = 1; c <= 9; c++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-			gold = num * word;
+			d = r * c;
 
-			if (gold <= 9)
+			if (d <= 9)
 				_putchar(' ');
 			else
-				_putchar(gold / 10) + '0');
+				_putchar((d / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+			_putchar((d % 10) + '0');
 		}
 		_putchar('\n');
 	}
+}	
