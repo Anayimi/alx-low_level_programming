23ac30551acfd922ca79387c4af632409c93a1cb
Anayimi <ogievaanayimi@gmail.com>
2022-09-14 03:55:51 -0700

    Prints every minute of the day

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,41 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table.
+ * times_table - prints the 9 times table
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void times_table(void)
 {
-	int num, word, gold;
+	int r, c, d;
 
-	for (num = 0; num <= 9; num++)
+	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
 
 		for (word = 1; word <= 9; word++)
 		{
