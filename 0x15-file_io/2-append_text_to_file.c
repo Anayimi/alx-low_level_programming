5195e6d3c858d52a0c72da6755418e33e6182406
Anayimi <ogievaanayimi@gmail.com>
2022-11-03 13:38:44 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,30 @@
-nclude <stdio.h>
 #include "main.h"
-
 /**
-  * append_text_to_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * append_text_to_file - This function adds text at EOF
+ * @filename: name of the file to append data to.
+ * @text_content: data to append to file
+ * Return: 1 if succesfull else -1 on err
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fild = 0, chk = 0, i = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	fild = open(filename, O_WRONLY | O_APPEND);
+	if (fild < 0)
 		return (-1);
 
 	if (text_content)
 	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
+		while (text_content[i])
+			i++;
+
+		chk = write(fild, text_content, i);
+		if (chk < 0)
+		return (-1);
 	}
-
-	close(fd);
+	close(fild);
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
